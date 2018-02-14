8d49d1f0bc3a7867c8a10746dafce07193e1386d
Bradley <brdeleeuw@gmail.com>
2018-02-14 15:26:57 +0100

    initial commit

---
+++ src/aws_functions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import subprocess as sp 
+import json 
+from pprint import pprint
+
+def create_log_with_all_ec2_instances(keypair):
+	sp.call("aws ec2 describe-instances --filters \"Name=key-name, Values={}\" > /home/bradley/slack_bot/src/logs/instances.json".format(keypair), shell=True)
+	data         = json.load(open("/home/bradley/slack_bot/src/logs/instances.json"))
+	reservations = data["Reservations"]
+	print("{} reservations found".format(len(reservations)))
+	return reservations
+
+def find_running_ec2_instances(reservations):
+	instances                = [instance for instance_list in [reservation.get("Instances") for reservation in reservations] for instance in instance_list]
+	running_instances        = [ (instance.get("InstanceId"), instance.get("State").get("Name")) for instance in instances if instance.get("State").get("Name") == "running"]
+	ids_of_running_instances = [ instance_id[0] for instance_id in running_instances]
+	no_of_running_instances  = len(running_instances)
+	print("{} instances found".format(len(instances)))
+	print("{} EC2 instance(s) currently running".format(no_of_running_instances))
+	return no_of_running_instances, ids_of_running_instances
+
---
+++ src/user_information/user_info.py
@@ -0,0 +1,6 @@
+
+last_names_and_keypairs = [ #{"last_name": "engelenhoven",
+						    #"keypair":  "nico_keypair"},
+						    {"last_name": "de leeuw",
+						    "keypair":  "bradley_dl_keypair"}]
+
