352120659288b4d3f1e69841115cc64a9cc7bebd
Bradley <brdeleeuw@gmail.com>
2018-02-14 16:15:23 +0100

    removing some cached files

--- src/user_information/user_info.py
+++
@@ -1,6 +0,0 @@
-
-last_names_and_keypairs = [ #{"last_name": "engelenhoven",
-						    #"keypair":  "nico_keypair"},
-						    {"last_name": "de leeuw",
-						    "keypair":  "bradley_dl_keypair"}]
-
